12bb09bdee7d0bd213ebf7ce2574070aef999cf0
Alí Guzmán Adán <aliguzmanadan@MacBook-Pro.local>
2021-10-13 11:58:46 +0200

    Entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +9,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    md_file = util.get_entry(title)
+    if bool(md_file):
+        html_file = markdown2.markdown(md_file)
+        return render(request, "encyclopedia/entry.html", {
+            "body": html_file,
+            "entry": title
+    })
+    return render(request, "encyclopedia/entry_error.html")
