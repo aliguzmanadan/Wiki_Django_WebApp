478c3c720cb5bc86b68509c5ce3b709a83096611
Alí Guzmán Adán <aliguzmanadan@MacBook-Pro.local>
2021-10-28 15:40:16 +0200

    Search entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,7 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 import markdown2
+from django.urls import reverse
 
 from . import util
 
@@ -18,3 +20,21 @@ def entry(request, title):
             "entry": title
     })
     return render(request, "encyclopedia/entry_error.html")
+
+def search(request):
+    #Getting query from Post methond in the form
+    query = request.POST["q"]   
+
+    #If the query matches an entry, then display the entry
+    if query in util.list_entries():
+        return entry(request, query)
+
+    #If the query does not matches an entry, display list with similar entries
+    similar_entries = [entry for entry in util.list_entries() if query in entry]
+    return render(request, "encyclopedia/search.html", {
+        "query": query,
+        "similar_entries": similar_entries
+    })
+
+def newpage(request):
+    return render(request, "encyclopedia/newpage.html")
